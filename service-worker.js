4cf08e7b90792d238c93b44078e54a1333cf077b
wolfimaffenpelz-web <wolfimaffenpelz@gmail.com>
2025-09-09 08:42:39 +0200

    feat: prompt user for service worker updates

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,47 @@
+const CACHE_NAME = "charakterbogen-cache-v2";
+const ASSETS = [
+  "/",              // Root
+  "/index.html",         // Hauptdokument
+  "/css/style-mobile.css",  // Styles Mobile
+  "/css/style-desktop.css", // Styles Desktop
+  "/js/sections.js",     // Struktur
+  "/js/translations.js", // Übersetzungen
+  "/js/logic.js",        // Logik
+  "/manifest.json",      // PWA Manifest
+  "/img/appicon.png",    // App-Icon
+  "/img/splash.png"      // Splashscreen
+];
+
 self.addEventListener("install", e => {
-  // Beim Installieren alle notwendigen Dateien cachen
+  self.skipWaiting();
   e.waitUntil(
-    caches.open("charakterbogen-cache").then(cache => {
-      return cache.addAll([
-        "/",              // Root
-        "/index.html",         // Hauptdokument
-        "/css/style-mobile.css",  // Styles Mobile
-        "/css/style-desktop.css", // Styles Desktop
-        "/js/sections.js",     // Struktur
-        "/js/translations.js", // Übersetzungen
-        "/js/logic.js",        // Logik
-        "/manifest.json",      // PWA Manifest
-        "/img/appicon.png",    // App-Icon
-        "/img/splash.png"      // Splashscreen
-      ]);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
 });
 
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clientsClaim();
+});
+
 self.addEventListener("fetch", e => {
-  // Versuche zuerst aus dem Cache zu antworten, ansonsten aus dem Netz
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(e.request, resClone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
+
+self.addEventListener("message", e => {
+  if (e.data && e.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
 });
